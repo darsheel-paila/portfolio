4470d39775df860a241534b427385f1e872f58f8
darsheel-paila <darsheelpaila@gmail.com>
2025-09-22 07:12:43 -0500

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,5 @@
 const icons = [
-      "fa-cog", "fa-robot", "fa-code", "fa-paintbrush", 
-      "fa-bolt", "fa-gears", "fa-wrench",
-      "fa-rocket", "fa-compass"
+      "fa-cog", "fa-code", "fa-paintbrush", 
     ];
 
     const container = document.querySelector(".background-icons");
@@ -11,11 +9,29 @@ const icons = [
       icon.className = `fa-solid ${icons[Math.floor(Math.random() * icons.length)]}`;
 
       // random position, size, speed, and delay
-      icon.style.left = Math.random() * 104 + "vw";
+      icon.style.left = Math.random() * 100 + "vw";
       icon.style.fontSize = 30 + Math.random() * 10 + "px";
-      icon.style.animationDuration = 10 + Math.random() * 20 + "s";
+      icon.style.animationDuration = 30 + Math.random() * 20 + "s";
       icon.style.animationDelay = Math.random() * 20 + "s";
-      icon.style.color = "rgba(255,255,255,0.2)";
+      icon.style.color = "rgba(255, 255, 255, 0.5)";
 
       container.appendChild(icon);
     }
+
+//image flexbox gallery
+
+    const images = document.querySelectorAll('.image');
+    const titleEl = document.getElement('data-title');
+    const descEl = document.getElement('data-desc');
+
+    images.forEach(img => {
+      img.addEventListener('mouseenter', () => {
+        titleEl.textContent = img.dataset.title;
+        descEl.textContent = img.dataset.desc;
+      });
+
+      img.addEventListener('mouseleave', () => {
+        titleEl.textContent = "Hover over a project";
+        descEl.textContent = "The description will appear here.";
+      });
+    });
